26209d43f5ff66f9dd70403900f7b502bd3068b0
João Pedro Padilha <jppadilha11@gmail.com>
2025-10-18 21:56:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7045633020-30e91",
+  "appId": "1:1086090220837:web:0bf334bfc4e90b9cd136cc",
+  "apiKey": "AIzaSyB0UR2Y-SAc8sHq-hcU56TL87wz9KbBNlM",
+  "authDomain": "studio-7045633020-30e91.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1086090220837"
+};
